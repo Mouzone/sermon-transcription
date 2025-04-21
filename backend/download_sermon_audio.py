4888877118235ddf0a00dde109d8ba7062ebcf1e
Mouzone <sunnyliu010@gmail.com>
2025-04-21 19:17:39 -0400

    Add .gitignore and modify yt download settings

--- backend/download_sermon_audio.py
+++ backend/download_sermon_audio.py
@@ -2,7 +2,17 @@ import yt_dlp
 
 
 def downloadSermonAudio(sermon_link: str) -> bool:
-    yt_opts = {"verbose": True, "force_keyframes_at_cuts": True, "audio": True}
+    yt_opts = {
+        "verbose": True,
+        "format": "bestaudio/best",
+        "outtmpl": "sermons/%(title)s.%(ext)s",
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+            }
+        ],
+    }
 
     with yt_dlp.YoutubeDL(yt_opts) as ydl:
         ydl.download("https://www.youtube.com/watch?v=BxUS1K7xu30")
