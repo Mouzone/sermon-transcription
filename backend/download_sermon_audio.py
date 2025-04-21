5f301e8bd7f91ce71a52f36afdf3febd1c2bd4cd
Mouzone <sunnyliu010@gmail.com>
2025-04-21 19:13:13 -0400

    Download audio stream of sermon

--- backend/download_sermon_audio.py
+++ backend/download_sermon_audio.py
@@ -1,34 +1,8 @@
-from pytubefix import YouTube
-from pytubefix.cli import on_progress
-import os
+import yt_dlp
 
 
 def downloadSermonAudio(sermon_link: str) -> bool:
-    try:
-        yt = YouTube(sermon_link, on_progress_callback=on_progress)
-        audio_stream = yt.streams.get_audio_only()
+    yt_opts = {"verbose": True, "force_keyframes_at_cuts": True, "audio": True}
 
-        if not audio_stream:
-            print("No audio stream found for this video.")
-            return False
-
-        print(f"Downloading from {sermon_link}")
-        downloaded_file = audio_stream.download("downloaded_sermons")
-
-        # Ensure the download directory exists
-        os.makedirs("downloaded_sermons", exist_ok=True)
-
-        base, _ = os.path.splitext(downloaded_file)
-        new_file = base + ".mp3"
-
-        # Handle case where new_file already exists
-        if os.path.exists(new_file):
-            os.remove(new_file)
-
-        os.rename(downloaded_file, new_file)
-        print(f"Successfully converted {yt.title} to MP3: {new_file}")
-        return True
-
-    except Exception as e:
-        print(f"Error downloading {sermon_link}: {str(e)}")
-        return False
+    with yt_dlp.YoutubeDL(yt_opts) as ydl:
+        ydl.download("https://www.youtube.com/watch?v=BxUS1K7xu30")
