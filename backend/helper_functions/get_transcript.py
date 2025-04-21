7515004cee7aac2278b0c3135beb855bf74e6080
Mouzone <sunnyliu010@gmail.com>
2025-04-21 19:43:01 -0400

    Add assembly ai audio transcription

---
+++ backend/helper_functions/get_transcript.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+import assemblyai as aai
+import os
+
+load_dotenv()
+
+
+def getTranscript():
+    aai.settings.api_key = os.getenv("ASSEMBLYAI_API_KEY")
+
+    transcriber = aai.Transcriber()
+    transcript = transcriber.transcribe(
+        "./sermons/Mon 4.21.25 Numbers 14： 1-10 The Limits of Leadership.mp3"
+    )
+
+    print(transcript.text)
