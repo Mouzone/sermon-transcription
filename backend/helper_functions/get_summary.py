243dc8df72d40b2a75cbabcfad12eef9c44e2ec8
Mouzone <sunnyliu010@gmail.com>
2025-04-21 20:15:26 -0400

    Create summary formatting using gemini

--- backend/helper_functions/get_summary.py
+++ backend/helper_functions/get_summary.py
@@ -1,12 +1,34 @@
 from google import genai
+from pydantic import BaseModel
+
+
+class Sermon(BaseModel):
+    anecdote: str
+    opening_prayer: str
+    bible_reading: str
+    sermon: str
+    closing_prayer: str
+    conclusion: str
 
 
 def getSummary(transcript):
 
     client = genai.Client(api_key="YOUR_API_KEY")
 
+    prompt = f"""
+        Given this sermon transcript: {transcript}. Split all the text into six parts:
+        opening anecdote, first prayer, bible passage reading, sermon, final prayer
+        and conclusion. Clean up the transcript removing any audio cues that are
+        unnnecessary, and do your best to clean it up to be readable and representable
+        without omitting too much espcially in sermon
+    """
     response = client.models.generate_content(
-        model="gemini-2.0-flash", contents="Explain how AI works in a few words"
+        model="gemini-2.0-flash",
+        contents=prompt,
+        config={
+            "response_mime_type": "application/json",
+            "response_schema": Sermon,
+        },
     )
 
     return response.text
