cb9ff5d2dd4a68b99fee880792a50a2711aa1e3e
Mouzone <sunnyliu010@gmail.com>
2025-04-21 20:28:36 -0400

    Refactor to use one file

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,74 @@
 from helper_functions.scrape_recent_sermon import scrapeRecentSermon
-from helper_functions.download_sermon_audio import downloadSermonAudio
-from helper_functions.get_transcript import getTranscript
-from backend.helper_functions.get_summary import getSummary
+from supabase import create_client, Client
 import asyncio
+import os
+from google import genai
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import assemblyai as aai
+import yt_dlp
+
+
+class Sermon(BaseModel):
+    anecdote: str
+    opening_prayer: str
+    bible_reading: str
+    sermon: str
+    closing_prayer: str
+    conclusion: str
+
+
+load_dotenv()
 
 
 async def main():
     sermon_data = await scrapeRecentSermon()
-    downloadSermonAudio(sermon_data["link"])
-    transcript = getTranscript()
-    summary = getSummary(transcript)
+
+    yt_opts = {
+        "verbose": True,
+        "format": "bestaudio/best",
+        "outtmpl": "sermons/%(title)s.%(ext)s",
+    }
+
+    with yt_dlp.YoutubeDL(yt_opts) as ydl:
+        ydl.download(sermon_data["link"])
+
+    aai.settings.api_key = os.getenv("ASSEMBLYAI_API_KEY")
+    transcriber = aai.Transcriber()
+
+    filename = os.listdir("../sermons")[0]
+    filepath = os.path.join("../sermons", filename)
+    transcript_obj = transcriber.transcribe(filepath)
+
+    transcript = transcript_obj.text
+
+    client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+    prompt = f"""
+        Given this sermon transcript: {transcript}. Split all the text into six parts:
+        opening anecdote, first prayer, bible passage reading, sermon, final prayer
+        and conclusion. Clean up the transcript removing any audio cues that are
+        unnnecessary, and do your best to clean it up to be readable and representable
+        without omitting too much espcially in sermon
+    """
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=prompt,
+        config={
+            "response_mime_type": "application/json",
+            "response_schema": Sermon,
+        },
+    )
+
+    summary = response.text
+
+    url = os.environ.get("SUPABASE_URL")
+    key = os.environ.get("SUPABASE_KEY")
+    supabase: Client = create_client(url, key)
+
+    # todo: check for response
+    # todo: add title from sermon_data to data to be inserted
+    supabase.table("Sermons").insert(summary).execute()
 
 
 if __name__ == "__main__":
