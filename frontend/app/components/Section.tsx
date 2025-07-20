fd75f8140074e64fc6639dac29ce9b9e76cb218a
Mouzone <sunnyliu010@gmail.com>
2025-07-19 21:16:41 -0400

    Render paragraphs on the frontend

--- frontend/app/components/Section.tsx
+++ frontend/app/components/Section.tsx
@@ -2,17 +2,23 @@ import { titleMappings } from "../utility.ts/consts";
 import { SectionLabel } from "../utility.ts/types";
 
 type SectionProps = {
-    title: SectionLabel;
-    value: string;
+	title: SectionLabel;
+	value: string;
 };
 
 export default function Section({ title, value }: SectionProps) {
-    return (
-        <div className="pb-4">
-            <h4 id={`${title}`} className="font-bold text-2xl pb-2">
-                {titleMappings[title]}
-            </h4>
-            <div className="indent-8">{value}</div>
-        </div>
-    );
+	const paragraphs: string[] = JSON.parse(value);
+	return (
+		<div>
+			<h4
+				id={`${title}`}
+				className="font-bold text-2xl pb-2"
+			>
+				{titleMappings[title]}
+			</h4>
+			{paragraphs.map((paragraph) => (
+				<div className="indent-8 pb-4">{paragraph}</div>
+			))}
+		</div>
+	);
 }
