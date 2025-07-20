ef2be516b748d2818f78a30f9a799bf6dfee881f
Mouzone <sunnyliu010@gmail.com>
2025-07-19 20:44:00 -0400

    Add new things from trying to run it live

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,38 +4,38 @@ import { sections } from "./utility.ts/consts";
 import supabase from "./utility.ts/supabase";
 
 export default async function Page() {
-    const { data, error } = await supabase
-        .from("Sermon")
-        .select("*")
-        .order("created_at", { ascending: false })
-        .limit(1)
-        .single();
+	const { data, error } = await supabase
+		.from("Sermon")
+		.select("*")
+		.order("created_at", { ascending: false })
+		.limit(1)
+		.single();
 
-    if (error) {
-        console.error("Error fetching recent data:", error);
-        return <></>;
-    }
+	if (error) {
+		console.error("Error fetching recent data:", error);
+		return <></>;
+	}
 
-    const [first, second, ...rest] = data["title"].split(" ");
-    const date = `${first} ${second}`;
-    const title = rest.join(" ");
+	const [first, second, ...rest] = data["title"].split(" ");
+	const date = `${first} ${second}`;
+	const title = rest.join(" ");
 
-    return (
-        <div className="p-12">
-            <div className="text-center text-xl pb-4">{date}</div>
-            <div className="font-bold text-4xl text-center pb-8">{title}</div>
-            <div className="flex justify-center gap-10 ">
-                <Nav />
-                <div className="flex flex-col gap-4 w-150">
-                    {sections.map((section) => (
-                        <Section
-                            key={section}
-                            title={section}
-                            value={data[section]}
-                        />
-                    ))}
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="p-12">
+			<div className="text-center text-xl pb-4">{date}</div>
+			<div className="font-bold text-4xl text-center pb-8">{title}</div>
+			<div className="flex justify-center gap-10 ">
+				<Nav />
+				<div className="flex flex-col gap-4 w-150">
+					{sections.map((section) => (
+						<Section
+							key={section}
+							title={section}
+							value={data[section]}
+						/>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
